c4f8770cc9dc4cf881b1d4ae92fa86cf14113e81
Kalpesh Badgujar <kalpeshbadgujarsvkm@gmail.com>
2025-05-17 19:06:48 +0530

    Fix CORS to allow all vercel.app

--- backend/app.js
+++ backend/app.js
@@ -20,7 +20,7 @@ app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', 'https://soen-main-subp.vercel.app');
     res.header('Access-Control-Allow-Credentials', 'true');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, accept, authorization');
     if (req.method === 'OPTIONS') {
         return res.sendStatus(200);
     }
